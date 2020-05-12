0d1d2beb47bf136dc1356884a36671e4e6b34f24
Weinsen <Weinsen@users.noreply.github.com>
2020-05-12 19:49:00 -0300

    Added vector and matrix layer connect

--- main.cpp
+++ main.cpp
@@ -10,25 +10,32 @@ int main()
 
 	Connector linker;
 
-	model.addLayer("l1").addLayer("l2").addLayer("l3").addLayer("l4");
+	model.addLayer("l1").addLayer("l2").addLayer("l3");
 
-	model.getLayer("l1").addInput("i1", 1).addInput("i2", 1);
-	model.getLayer("l2").addNeurons(50);
-	model.getLayer("l3").addNeurons(50);
-	model.getLayer("l4").addNamed("o1").addNamed("o2");
+	model.getLayer("l1").addInput("i1", 1).addInput("i2", 1).addInput("i3", -3).addInput("i4", -4);
+	model.getLayer("l2").addNeurons(10);
+	model.getLayer("l3").addNamed("o1").addNamed("o2");
 
-	linker.connect(model, "l2", "l1");
+	std::vector<std::vector<double>> w = {
+		{ 0,  1, -1,  2},
+		{ 0, -1,  1,  2},
+		{ 3,  0,  0, -2},
+		{-2,  1, -1,  2},
+		{ 2,  0, -2,  2},
+		{ 0,  1, -1,  2},
+		{ 0,  1, -1,  2},
+		{ 8,  0, -5,  2},
+		{-1,  1, -1,  2},
+		{ 0,  1, -1,  2}
+	};
+
+	linker.connect(model, "l2", "l1", &w);
 	linker.connect(model, "l3", "l2");
-	linker.connect(model, "l4", "l3");
 
 	model.calculate();
-	std::cout << model.getLayer("l4");
 
-	model.getNamedNeuron("i2").set(5);
-	model.getNamedNeuron("i1").set(-1);
-
-	model.calculate();
-	std::cout << model.getLayer("l4");
+	std::cout << model.getLayer("l2");
+	std::cout << model.getLayer("l3");
 
 
 	return 0;
